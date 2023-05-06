a5074dffe0f5a206799c8fb005041c3e39106175
Polukonda Chinmaya <pchinmaya@tataunistore.com>
2023-05-06 17:39:18 +0530

    React is added

---
+++ src/client/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+// export default class App extends Component {
+//   render() {
+//     return <div>Welcome to React Boilerplate App</div>;
+//   }
+// }
+
+export default ()=>{
+
+      return <div>Welcome to React Boilerplate App</div>;
+
+  }
